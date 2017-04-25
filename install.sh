8caccfa637def9707cbefca2550421b27049c081
charlie <claycbr600@gmail.com>
2017-04-25 17:19:51 -0400

    check if Vundle already installed

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+# link dotfiles
 files=('gemrc' 'vimrc' 'zprofile' 'zshrc');
 for file in "${files[@]}"; do
   echo "linking $HOME/.dotfiles/$file -> $HOME/.$file"
   ln -sf $HOME/.dotfiles/$file $HOME/.$file
 done
+
+# clone Vundle
+if [ -d $HOME/.vim/bundle/Vundle.vim ]; then
+  echo "Vundle already installed"
+else
+  echo "cloning vundle to $HOME/.vim/bundle/Vundle.vim"
+  git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+fi
