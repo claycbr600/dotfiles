05836320245ad5f0c6bb45d6bf2ef263a1e454f8
charlie <claycbr600@gmail.com>
2017-04-25 17:12:18 -0400

    remove invalid ln flag

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 files=('gemrc' 'vimrc' 'zprofile' 'zshrc');
 for file in "${files[@]}"; do
   echo "linking $HOME/.dotfiles/$file -> $HOME/.$file"
-  ln -sfh $HOME/.dotfiles/$file $HOME/.$file
+  ln -sf $HOME/.dotfiles/$file $HOME/.$file
 done
